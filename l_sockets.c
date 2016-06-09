7f931eeb63d2182a69ff799e4b477e9f7fb1d386
Carlos Gómez Cainzos <carlos.gomezcainzos@gmail.com>
2016-06-09 08:58:00 +0200

    more thinhs

--- l_sockets.c
+++ l_sockets.c
@@ -3,6 +3,28 @@
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 
+int xstrsearch ( char * s1, char * s2 )
+{
+    int i, j, k ;
+    int l1 = strlen ( s1 ) ;
+    int l2 = strlen ( s2 ) ;
+
+    for ( i = 0 ; i <= l1 - l2 ; i++ )
+    {
+        j = 0 ;
+        k = i ;
+        while ( ( s1[k] == s2[j] ) && ( j < l2 ) )
+        {
+            k++ ;
+            j++ ;
+        }
+        if ( j == l2 )
+            return i ;
+    }
+    return -1 ;
+}
+
+
 char * http_tokenize(char * message)
 {
     char *tkn_message = message;
@@ -19,9 +41,9 @@ char * http_tokenize(char * message)
 }
 
 int main(int argc, char *argv[]) {
-	int socket_desc;
-        char *message , server_reply[3000];
-        char *date;
+	int socket_desc, time_header_pos;
+    char *message , server_reply[3000];
+    char date[11];
 
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -59,11 +81,17 @@ int main(int argc, char *argv[]) {
 	puts("Reply received\n");
 	//puts(server_reply);
 
-        //printf(http_tokenize(server_reply));
-        http_tokenize(server_reply);
+        // printf(http_tokenize(server_reply));
+        // http_tokenize(server_reply);
+    time_header_pos = xstrsearch(server_reply, "Posix-time:");
+    printf("Postion: %d", time_header_pos);
+    for(int i=0; i < 10; i++) {
+        date[i] = server_reply[time_header_pos + 12 + i];
+    }
+ 
 
-        date = strstr(server_reply, "\r\n\r\n");
-        printf("\nDate from server: %s\n", date);
+    //date = strstr(server_reply, "\r\n\r\n");
+    printf("\nDate from server: %s\n", date);
      
 	return 0;
 
